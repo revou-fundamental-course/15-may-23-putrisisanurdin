0adb27dfa5dba4561107b967525a5b7d72d48dae
Putri Sisa Nurdin <putrisisanurdin@gmail.com>
2023-05-23 21:11:17 +0700

    add some javascript

---
+++ js/javascripst.js
@@ -0,0 +1,7 @@
+function hitungLuas() {
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    document.getElementsByClassName("result-luas").style = "visible";
+    console.log(sisiLuas);
+    document.getElementById("output-luas").innerHTML  = sisiLuas
+} 
